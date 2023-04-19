73f509c58088b190301260afc0b63027266ed863
Szymon Wójcik <wojcik.szymon1@gmail.com>
2023-04-19 15:30:28 +0200

    refactor: adjust components to use global state from provider

--- lib/providers/favorites_provider.dart
+++ lib/providers/favorites_provider.dart
@@ -4,18 +4,20 @@ import 'package:meals/models/meal.dart';
 class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
   FavoriteMealsNotifier() : super([]);
 
-  void toggleMealFavoriteStatus(Meal meal) {
+  bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
 
     if (mealIsFavorite) {
       state = state.where((element) => element.id != meal.id).toList();
+      return false;
     } else {
       state = [...state, meal];
+      return true;
     }
   }
 }
 
-final favoritesProvider =
+final favoritesMealsProvider =
     StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>(
   (ref) {
     return FavoriteMealsNotifier();
